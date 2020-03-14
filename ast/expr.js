ac75ea12c900677aa130349ff69facaa76382a2e
andrea <andrea.fiori.1998@gmail.com>
2020-03-14 10:45:06 +0000

    make AST data structure

--- ast/expr.js
+++ ast/expr.js
@@ -1,36 +1,67 @@
-exports.makeBinaryExpr = function(left, operator, right) {
-    return {
-        left, operator, right,
-        accept(visitor) {
-            return visitor.visitBinaryExpr(this);
-        }
-    };
-};
+exports.Binary = class {
+    constructor(left, operator, right) {
+        this.left = left;
+        this. operator =  operator;
+        this. right =  right;
+    }
 
-exports.makeUnaryExpr = function(operator, right) {
-    return {
-        operator, right,
-        accept(visitor) {
-            return visitor.visitUnaryExpr(this);
-        }
-    };
-};
+    accept(visitor) {
+        return visitor.visitBinaryExpr(this);
+    }
+}
 
-exports.makeLiteralExpr = function(value) {
-    return {
-        value,
-        accept(visitor) {
-            return visitor.visitLiteralExpr(this);
-        }
-    };
-};
+exports.Unary = class {
+    constructor(operator, right) {
+        this.operator = operator;
+        this. right =  right;
+    }
 
-exports.makeGroupingExpr = function(expression) {
-    return {
-        expression,
-        accept(visitor) {
-            return visitor.visitGroupingExpr(this);
-        }
-    };
-};
+    accept(visitor) {
+        return visitor.visitUnaryExpr(this);
+    }
+}
+
+exports.Literal = class {
+    constructor(value) {
+        this.value = value;
+    }
+
+    accept(visitor) {
+        return visitor.visitLiteralExpr(this);
+    }
+}
+
+exports.Grouping = class {
+    constructor(expression) {
+        this.expression = expression;
+    }
+
+    accept(visitor) {
+        return visitor.visitGroupingExpr(this);
+    }
+}
+
+exports.Ternary = class {
+    constructor(first, second, third) {
+        this.first = first;
+        this. second =  second;
+        this. third =  third;
+    }
+
+    accept(visitor) {
+        return visitor.visitTernaryExpr(this);
+    }
+}
+
+exports.Logical = class {
+    constructor(left, operator, right) {
+        this.left = left;
+        this. operator =  operator;
+        this. right =  right;
+    }
+
+    accept(visitor) {
+        return visitor.visitLogicalExpr(this);
+    }
+}
 
