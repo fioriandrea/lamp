17908883a812f48446cd0f8b26ac4d0bf058cb1b
andrea <andrea.fiori.1998@gmail.com>
2020-03-16 00:12:29 +0000

    add typeof builtin

--- backend/builtins.js
+++ backend/builtins.js
@@ -16,4 +16,24 @@ class Len {
     }
 }
 
-module.exports = [new Len()];
+class Typeof {
+    arity() {
+        return 1;
+    }
+
+    call(interpeter, args) {
+        const arg = args[0];
+        if (arg === null) return 'nihl';
+        else if (util.isString(arg)) return 'string';
+        else if (util.isNumber(arg)) return 'number';
+        else if (util.isArray(arg)) return 'array';
+        else if (util.isMap(arg)) return 'map';
+        else return 'undefined_type';
+    }
+
+    name() {
+        return 'typeof';
+    }
+}
+
+module.exports = [new Len(), new Typeof()];
